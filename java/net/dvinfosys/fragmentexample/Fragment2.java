247962f7ca4a885c204314656e6faeff5042c14f
DV Infosys <dvinfosys0@gmail.com>
2017-11-16 23:34:40 +0530

    Create Fragment2.java

---
+++ java/net/dvinfosys/fragmentexample/Fragment2.java
@@ -0,0 +1,17 @@
+package net.dvinfosys.fragmentexample;  
+  
+import android.app.Fragment;  
+import android.os.Bundle;  
+import android.view.LayoutInflater;  
+import android.view.View;  
+import android.view.ViewGroup;  
+  
+public class Fragment2 extends Fragment {  
+      
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  
+            Bundle savedInstanceState) {  
+        // TODO Auto-generated method stub  
+        return inflater.inflate(R.layout.fragment2,container, false);  
+    }  
+  
+}  
