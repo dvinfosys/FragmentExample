d4861e833049baa54bdfa8153e1b838e32ff94a3
DV Infosys <dvinfosys0@gmail.com>
2017-11-16 23:24:40 +0530

    Update and rename .gitignore to java/MainActivity.java

---
+++ java/MainActivity.java
@@ -0,0 +1,51 @@
+package com.abhiandroid.fragmentexample;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+Button firstFragment, secondFragment;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+// get the reference of Button's
+firstFragment = (Button) findViewById(R.id.firstFragment);
+secondFragment = (Button) findViewById(R.id.secondFragment);
+
+// perform setOnClickListener event on First Button
+firstFragment.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+// load First Fragment
+loadFragment(new FirstFragment());
+}
+});
+// perform setOnClickListener event on Second Button
+secondFragment.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+// load Second Fragment
+loadFragment(new SecondFragment());
+}
+});
+
+}
+
+private void loadFragment(Fragment fragment) {
+// create a FragmentManager
+FragmentManager fm = getFragmentManager();
+// create a FragmentTransaction to begin the transaction and replace the Fragment
+FragmentTransaction fragmentTransaction = fm.beginTransaction();
+// replace the FrameLayout with new Fragment
+fragmentTransaction.replace(R.id.frameLayout, fragment);
+fragmentTransaction.commit(); // save the changes
+}
+}
